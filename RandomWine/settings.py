5a0cdc1badc8225fe9ba52aa185986c06b35248e
jucastedova <100007157.joan23@fje.edu>
2021-08-17 12:05:17 +0200

    preparando para heroku

--- RandomWine/settings.py
+++ RandomWine/settings.py
@@ -48,6 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # NEW
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # END NEW
 ]
 
 ROOT_URLCONF = 'RandomWine.urls'
@@ -129,6 +132,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+# NEW
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# END NEW
 
 STATIC_URL = '/static/'
 
